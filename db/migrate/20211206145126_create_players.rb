69ff4e3562dd9e1783fc57b4bbd75edbb3741552
+ <brandi.ude@gmail.com>
2021-12-06 10:44:32 -0500

    tables created

---
+++ db/migrate/20211206145126_create_players.rb
@@ -0,0 +1,9 @@
+class CreatePlayers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
-puts "🌱 Seeding spices..."
+puts "Deleting old data..."
+Player.destroy_all
+Score.destroy_all
+
+puts "🌱 Seeding players..."
 
 # Seed your database here
+p1 = Player.create(first_name:"Brandi", last_name: "Ude", age: 28 )
+
+puts "🌱 Seeding scores..."
+
+Score.create(player_id:pa1.id, score: )
 
 puts "✅ Done seeding!"
