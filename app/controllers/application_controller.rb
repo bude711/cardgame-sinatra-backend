f12e0ec239c3a661b1779173bd37d6c3794799e1
+ <brandi.ude@gmail.com>
2021-12-08 14:13:25 -0500

    added some sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,51 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/scores' do
+    scores = Score.all
+    scores.to_json
+  end
+  
+  # get '/bakeries/:id' do
+  #   bakery = Bakery.find(params[:id])
+  #   bakery.to_json(include: :baked_goods)
+  # end
+
+  # get '/baked_goods/by_price' do
+  #   # see the BakedGood class for the  method definition of `.by_price`
+  #   baked_goods = BakedGood.by_price
+  #   baked_goods.to_json
+  # end
+
+  # get '/baked_goods/most_expensive' do
+  #   # see the BakedGood class for the  method definition of `.by_price`
+  #   baked_good = BakedGood.by_price.first
+  #   baked_good.to_json
+  # end
+
+ 
+  post '/player' do
+    player_info = Player.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      age: params[:age]
+    )
+    player_info.to_json
+  end
+
+  patch '/players/:id' do
+    player = Player.find(params[:id])
+    player.update(first_name: params[:first_name])
+    player.update(last_name: params[:last_name])
+    player.update(age: params[:age])
+    player.to_json
+  end
+
+  delete '/score/:id' do
+    score = Score.find(params[:id])
+    score.destroy
+    score.to_json
+  end
+
+
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,12 @@
 class Score < ActiveRecord::Base
     belongs_to :player
+
+    def self.highest_score
+        Score.all.order(score: :desc).first
+    end
+
+    def highest_to_lowest
+        Score.all.order(score: :desc)
+    end
+
 end
