ee76d4aca12a683e1e6a3b2685d6f8f607b56152
+ <brandi.ude@gmail.com>
2021-12-08 17:31:24 -0500

    made some tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,12 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
+# only need to do (at least) 3 of the CRUD actions for this project
   get '/scores' do
     scores = Score.all
     scores.to_json
   end
-  
-  # get '/bakeries/:id' do
-  #   bakery = Bakery.find(params[:id])
-  #   bakery.to_json(include: :baked_goods)
-  # end
 
-  # get '/baked_goods/by_price' do
-  #   # see the BakedGood class for the  method definition of `.by_price`
-  #   baked_goods = BakedGood.by_price
-  #   baked_goods.to_json
-  # end
-
-  # get '/baked_goods/most_expensive' do
-  #   # see the BakedGood class for the  method definition of `.by_price`
-  #   baked_good = BakedGood.by_price.first
-  #   baked_good.to_json
-  # end
-
- 
   post '/player' do
     player_info = Player.create(
       first_name: params[:first_name],
@@ -46,11 +24,31 @@ class ApplicationController < Sinatra::Base
     player.to_json
   end
 
-  delete '/score/:id' do
-    score = Score.find(params[:id])
-    score.destroy
-    score.to_json
-  end
-
-
 end
+
+  # OTHER EXAMPLES BELOW FROM LABS:
+
+  # get '/bakeries/:id' do
+  #   bakery = Bakery.find(params[:id])
+  #   bakery.to_json(include: :baked_goods)
+  # end
+
+  # get '/baked_goods/by_price' do
+  #   # see the BakedGood class for the  method definition of `.by_price`
+  #   baked_goods = BakedGood.by_price
+  #   baked_goods.to_json
+  # end
+
+  # get '/baked_goods/most_expensive' do
+  #   # see the BakedGood class for the  method definition of `.by_price`
+  #   baked_good = BakedGood.by_price.first
+  #   baked_good.to_json
+  # end
+
+  # delete '/baked_goods/:id' do
+  #   baked_good = BakedGood.find(params[:id])
+  #   baked_good.destroy
+  #   baked_good.to_json
+  # end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ puts "🌱 Seeding scores..."
 
 Score.create(player_id:p1.id, score: 5)
 Score.create(player_id:p1.id, score: 7)
-Score.create(player_id:p1.id, score: 3)
+Score.create(player_id:p1.id, score: 11)
+Score.create(player_id:p1.id, score: 15)
+Score.create(player_id:p1.id, score: 21)
 
 puts "✅ Done seeding!"
